1a4cfe5ff151d099e61344221f840b66a86632c9
Candace <2100978@talnet.nl>
2020-03-04 10:17:10 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+var p = document.querySelectorAll("p");
+p[0].classList.add("Txt1");
+
+p[0].innerText += " ik ging op reis";
+p[1].innerText += " en ik nam mee";
+p[2].innerText += " een stekkerdoos";
+p[3].innerText += " en een zaklamp";
